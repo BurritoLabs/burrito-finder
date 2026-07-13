0373a62763630f6be64822024f237cf18379f8dc
J Feng <fengzhun2011@gmail.com>
2026-07-13 14:29:23 -0400

    fix: stop probing missing classic token icons

--- src/pages/Validator/Rewards.tsx
+++ src/pages/Validator/Rewards.tsx
@@ -25,8 +25,6 @@ const Rewards = ({ title, list }: { title: string; list: Coin[] }) => {
   const ibcWhitelist = useIBCWhitelist(ibcDenoms);
   const ibcFallbackIcon =
     "https://raw.githubusercontent.com/terra-money/assets/master/icon/svg/IBC.svg";
-  const cwFallbackIcon =
-    "https://raw.githubusercontent.com/terra-money/assets/master/icon/svg/CW.svg";
   const fallbackIcon =
     "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='64' height='64'><rect width='64' height='64' rx='32' fill='%23304036'/><path d='M32 18a14 14 0 100 28 14 14 0 000-28z' fill='%235a6b65'/></svg>";
 
@@ -56,18 +54,9 @@ const Rewards = ({ title, list }: { title: string; list: Coin[] }) => {
                 const displayDenom = isIbc
                   ? renderIbcDenom(denom, ibcInfo, isClassic)
                   : format.denom(denom, isClassic);
-                const classicIconDenom =
-                  denom === "uluna"
-                    ? "LUNC"
-                    : format.denom(denom, false).toUpperCase();
-                const mainnetIconDenom =
-                  denom === "uluna" ? "Luna" : displayDenom;
-                const iconDenom = isClassic
-                  ? classicIconDenom
-                  : mainnetIconDenom;
-                const classicStable =
-                  isClassic && format.denom(denom, true).endsWith("TC");
+                const iconDenom = denom === "uluna" ? "Luna" : displayDenom;
                 const isMainnetLuna = !isClassic && denom === "uluna";
+                const isClassicNative = isClassic && !isIbc;
                 const lunaIcons = [
                   `${ASSET_URL}/icon/svg/Luna.svg`,
                   `${ASSET_URL}/icon/svg/LUNA.svg`,
@@ -78,7 +67,13 @@ const Rewards = ({ title, list }: { title: string; list: Coin[] }) => {
                   ibcInfo?.icon,
                   ...(isIbc ? [ibcFallbackIcon] : []),
                   ...(isMainnetLuna ? lunaIcons : []),
-                  ...(isMainnetLuna
+                  ...(isClassicNative
+                    ? [
+                        `${ASSET_URL}/icon/svg/${
+                          denom === "uluna" ? "LUNC" : "USTC"
+                        }.svg`
+                      ]
+                    : isMainnetLuna || isIbc
                     ? []
                     : [
                         `${ASSET_URL}/icon/svg/${iconDenom}.svg`,
@@ -86,18 +81,8 @@ const Rewards = ({ title, list }: { title: string; list: Coin[] }) => {
                         `${ASSET_URL}/icon/60/${String(
                           iconDenom
                         ).toLowerCase()}.png`
-                      ]),
-                  ...(classicStable
-                    ? [
-                        `${ASSET_URL}/icon/svg/USTC.svg`,
-                        `${ASSET_URL}/icon/60/USTC.png`,
-                        `${ASSET_URL}/icon/60/ustc.png`
-                      ]
-                    : [])
+                      ])
                 ].filter(Boolean) as string[];
-                if (!isIbc) {
-                  iconCandidates.push(cwFallbackIcon);
-                }
                 iconCandidates.push(fallbackIcon);
 
                 return (
